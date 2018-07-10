0feb3ad3e84bfdea124da25a9427613e1602599d
Raphael Ugha <raphaelugha@gmail.com>
2018-07-10 15:23:50 +0100

    added two instances of bounty

--- console.rb
+++ console.rb
@@ -1,2 +1,5 @@
 require('pry-byebug')
 require_relative('model/bounty')
+
+bounty1 = Bounty.new({'name' => 'JarJar', 'bounty_value' => '1000', 'danger_level' => 'medium', 'favourite_weapon' => 'space rifle'})
+bounty2 = Bounty.new({'name' => 'Jabba The Hutt', 'bounty_value' => '2500', 'danger_level' => 'high', 'favourite_weapon' => 'cosmic granade'})
