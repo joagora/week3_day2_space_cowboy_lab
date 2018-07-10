c72da0406e9b483c3140d6e89e4e37ba1f760b2b
Raphael Ugha <raphaelugha@gmail.com>
2018-07-10 15:49:21 +0100

    ran all and delete methods

--- console.rb
+++ console.rb
@@ -3,3 +3,12 @@ require_relative('model/bounty')
 
 bounty1 = Bounty.new({'name' => 'JarJar', 'bounty_value' => '1000', 'danger_level' => 'medium', 'favourite_weapon' => 'space rifle'})
 bounty2 = Bounty.new({'name' => 'Jabba The Hutt', 'bounty_value' => '2500', 'danger_level' => 'high', 'favourite_weapon' => 'cosmic granade'})
+
+
+bounty1.save
+bounty2.save
+
+all_bounty = Bounty.all
+p all_bounty
+
+bounty1.delete
