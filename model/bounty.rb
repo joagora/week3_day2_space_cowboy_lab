0595fddd64c6eeaf45c74486ed3a7e6b45df980e
Raphael Ugha <raphaelugha@gmail.com>
2018-07-10 15:34:23 +0100

    completed save method

--- model/bounty.rb
+++ model/bounty.rb
@@ -5,10 +5,24 @@ class Bounty
   def initialize attributes
     @id = attributes['id'].to_i if attributes['id']
     @name = attributes['name']
-    @bounty_value = attributes['bounty_value']
+    @bounty_value = attributes['bounty_value'].to_i
     @danger_level = attributes['danger_level']
     @favourite_weapon = attributes['favourite_weapon']
   end
 
+  def save
+    #connect
+    db = PG.connect({dbname: 'bounty', host: 'localhost'})
+    #write with actual details
+    sql = "INSERT INTO bounty (name, bounty_value, danger_level, favourite_weapon) VALUES ($1, $2, $3, $4) RETURNING *;"
+    values = [@name, @bounty_value, @danger_level, @favourite_weapon]
+    #prepare statements
+    db.prepare("save", sql)
+    #execute sql
+    result = db.exec_prepared("save", values)
+    @id = result.first['id'].to_i
+    #close connect
+    db.close
+  end
 
 end
